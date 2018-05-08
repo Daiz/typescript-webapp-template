e1de3d6e0c0e20ddc1de1fb474eb8544618631e1
Daiz <taneli.vatanen@gmail.com>
2018-05-08 20:20:32 +0300

    Add module example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
-const message = "Hello from Webpack & TypeScript!";
+import { normalize, setupPage } from "csstips";
+import { rem } from "csx";
+import { style } from "typestyle";
+import { greeting } from "./message";
+
+normalize();
+setupPage("#root");
+
+const fancyText = style({
+  margin: rem(1),
+  fontSize: rem(2),
+  color: "teal"
+});
+
+const message = greeting("Webpack & TypeScript");
 const d = document;
-const el = d.querySelector("#root")!;
+const el = d.querySelector("h1")!;
+el.className = fancyText;
 el.textContent = message;
 
 console.log(message);
