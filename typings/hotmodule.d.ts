164cc7d8c5228898083a68f8c194c4ce3afdeb34
Daiz <taneli.vatanen@gmail.com>
2018-05-09 19:24:19 +0300

    Add custom typings for hot module reloading

---
+++ typings/hotmodule.d.ts
@@ -0,0 +1,87 @@
+// Typings for Hot Module Reloading
+declare namespace HotModule {
+  interface HotNotify {
+    type:
+      | "self-declined"
+      | "declined"
+      | "unaccepted"
+      | "accepted"
+      | "disposed"
+      | "accept-errored"
+      | "self-accept-errored"
+      | "self-accept-error-handler-errored";
+    moduleId: number;
+  }
+
+  interface HotNotifyDeclined extends HotNotify {
+    type: "declined";
+    chain: number[];
+    parentId: number;
+  }
+
+  interface HotNotifyUnaccepted extends HotNotify {
+    type: "unaccepted";
+    chain: number[];
+  }
+
+  interface HotNotifyAccepted extends HotNotify {
+    type: "accepted";
+    chain: number[];
+    outdatedModules: number[];
+    outdatedDependencies: {
+      [key: number]: number[];
+    };
+  }
+
+  interface HotNotifyDisposed extends HotNotify {
+    type: "disposed";
+  }
+
+  interface HotNotifyErrored extends HotNotify {
+    type:
+      | "accept-errored"
+      | "self-accept-errored"
+      | "self-accept-error-handler-errored";
+    dependencyId: number;
+    error: Error;
+    originalError?: Error;
+  }
+
+  interface HotApplyOptions {
+    ignoreUnaccepted?: boolean;
+    ignoreDeclined?: boolean;
+    ignoreErrored?: boolean;
+    onDeclined(callback: (info: HotNotifyDeclined) => void): void;
+    onUnaccepted(callback: (info: HotNotifyUnaccepted) => void): void;
+    onAccepted(callback: (info: HotNotifyAccepted) => void): void;
+    onDisposed(callback: (info: HotNotifyDisposed) => void): void;
+    onErrored(callback: (info: HotNotifyErrored) => void): void;
+  }
+
+  type Status =
+    | "idle"
+    | "check"
+    | "prepare"
+    | "ready"
+    | "dispose"
+    | "apply"
+    | "abort"
+    | "fail";
+
+  interface Hot {
+    accept(dependencies: string | string[], callback: () => void): void;
+    decline(dependencies: string | string[]): void;
+    dispose(callback: (data: any) => void): void;
+    addDisposeHandler(callback: (data: any) => void): void;
+    removeDisposeHandler(callback: (data: any) => void): void;
+    status(): Status;
+    check(autoApply: boolean | HotApplyOptions): Promise<number[]>;
+    apply(options: HotApplyOptions): Promise<number[]>;
+    addStatusHandler(callback: (status: Status) => void): void;
+    removeStatusHandler(callback: (status: Status) => void): void;
+  }
+}
+
+declare interface NodeHotModule extends NodeModule {
+  hot?: HotModule.Hot;
+}
