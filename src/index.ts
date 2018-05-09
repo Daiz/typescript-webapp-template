952310cd3e9d4167d343d2a2ac8bd4bf6337a7e6
Daiz <taneli.vatanen@gmail.com>
2018-05-09 19:25:47 +0300

    Remove unnecessary imports from example index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { normalize, setupPage } from "csstips";
-import { rem } from "csx";
-import { style } from "typestyle";
 import greeting from "./message";
 
 const ROOT = "#root";
@@ -8,12 +6,6 @@ const NAMES = "Webpack & TypeScript";
 
 normalize();
 setupPage(ROOT);
-
-const fancyText = style({
-  margin: rem(1),
-  fontSize: rem(2),
-  color: "teal"
-});
 
 const d = document;
 const el = d.querySelector(ROOT)!;
